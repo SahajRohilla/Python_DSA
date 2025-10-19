a68ab65136990ae998faa5b26ee228637e28e083
programminGuy <sahajrohilla@gmail.com>
2025-10-19 22:46:12 +0530

    Singly linkedid List

---
+++ LinkedList/singlyLinkedList.py
@@ -0,0 +1,21 @@
+# class Node:
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+# class LinkedList:
+class LinkedList:
+    def __init__(self,value):
+        self.new_node = Node(value)
+        self.head = self.new_node
+        self.tail = self.new_node
+        self.length = 1
+
+my_linked_list = LinkedList(4)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+
+                                                                                                                    
