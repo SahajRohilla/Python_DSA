61343be4f443973367bc65069ff45439ac157a50
programminGuy <sahajrohilla@gmail.com>
2025-10-19 23:17:49 +0530

    append

--- LinkedList/singlyLinkedList.py
+++ LinkedList/singlyLinkedList.py
@@ -12,10 +12,40 @@ class LinkedList:
         self.tail = self.new_node
         self.length = 1
 
-my_linked_list = LinkedList(4)
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        
+        self.length += 1
+
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.make_empty()
+
+my_linked_list.append(3)
+my_linked_list.append(2)
 
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length)
+print('Length:', my_linked_list.length, '\n')
 
-                                                                                                                    
+print('Linked List:')
+my_linked_list.print_list()
